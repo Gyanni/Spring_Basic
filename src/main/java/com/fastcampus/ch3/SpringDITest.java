7ebddd1e4899c5336e132f4a3b6759bddaafe2f9
Lee Gi-Hwan <lghlee5568@naver.com>
2022-12-29 17:16:10 +0900

    Fix

--- src/main/java/com/fastcampus/ch3/SpringDITest.java
+++ src/main/java/com/fastcampus/ch3/SpringDITest.java
@@ -1,70 +1,70 @@
-package com.fastcampus.ch3;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
-import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-
-@Component class Engine {}
-@Component class Door {}
-@Component class Car {
-    @Value("black") String color;
-    @Value("100") int oil;
-    @Autowired Engine engine;
-    @Autowired Door[] doors;
-
-    public Car() {} // 기본 생성자는 반드시 만들어 주는 편이 좋다.
-
-    public Car(String color, int oil, Engine engine, Door[] doors) {
-        this.color = color;
-        this.oil = oil;
-        this.engine = engine;
-        this.doors = doors;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setOil(int oil) {
-        this.oil = oil;
-    }
-
-    public void setEngine(Engine engine) {
-        this.engine = engine;
-    }
-
-    public void setDoors(Door[] doors) {
-        this.doors = doors;
-    }
-
-    @Override
-    public String toString() {
-        return "Car{" +
-                "color='" + color + '\'' +
-                ", oil=" + oil +
-                ", engine=" + engine +
-                ", doors=" + Arrays.toString(doors) +
-                '}';
-    }
-}
-
-public class SpringDITest {
-    public static void main(String[] args) {
-        ApplicationContext ac = new GenericXmlApplicationContext("config.xml");
-        Car car = (Car)ac.getBean("car");
-
-        Engine engine = (Engine)ac.getBean("engine");
-        Door door = (Door)ac.getBean("door");
-
-//        car.setColor("black");
-//        car.setOil(100);
-//        car.setEngine(engine);
-//        car.setDoors(new Door[]{ac.getBean("door", Door.class), ac.getBean("door", Door.class)});
-
-        System.out.println("car = " + car);
-    }
-}
+//package com.fastcampus.ch3;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.GenericXmlApplicationContext;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Arrays;
+//
+//@Component class Engine {}
+//@Component class Door {}
+//@Component class Car {
+//    @Value("black") String color;
+//    @Value("100") int oil;
+//    @Autowired Engine engine;
+//    @Autowired Door[] doors;
+//
+//    public Car() {} // 기본 생성자는 반드시 만들어 주는 편이 좋다.
+//
+//    public Car(String color, int oil, Engine engine, Door[] doors) {
+//        this.color = color;
+//        this.oil = oil;
+//        this.engine = engine;
+//        this.doors = doors;
+//    }
+//
+//    public void setColor(String color) {
+//        this.color = color;
+//    }
+//
+//    public void setOil(int oil) {
+//        this.oil = oil;
+//    }
+//
+//    public void setEngine(Engine engine) {
+//        this.engine = engine;
+//    }
+//
+//    public void setDoors(Door[] doors) {
+//        this.doors = doors;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Car{" +
+//                "color='" + color + '\'' +
+//                ", oil=" + oil +
+//                ", engine=" + engine +
+//                ", doors=" + Arrays.toString(doors) +
+//                '}';
+//    }
+//}
+//
+//public class SpringDITest {
+//    public static void main(String[] args) {
+//        ApplicationContext ac = new GenericXmlApplicationContext("config.xml");
+//        Car car = (Car)ac.getBean("car");
+//
+//        Engine engine = (Engine)ac.getBean("engine");
+//        Door door = (Door)ac.getBean("door");
+//
+////        car.setColor("black");
+////        car.setOil(100);
+////        car.setEngine(engine);
+////        car.setDoors(new Door[]{ac.getBean("door", Door.class), ac.getBean("door", Door.class)});
+//
+//        System.out.println("car = " + car);
+//    }
+//}
